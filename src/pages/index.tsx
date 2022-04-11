22e5a0139f9dd38a9699b2d47d47c1a0ddf1f4c0
leobtt <leonarbat8@gmail.com>
2022-04-10 22:03:21 -0300

    global style + header

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Ínicio | Ig.news</title>
+    </Head>
+    <div>
+      <h1 >Hello World!!</h1>
+    </div>
+    </>
+  )
+}
