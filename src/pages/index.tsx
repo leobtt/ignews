0609cf7e37886bc49dd7312e35353d16be27d43a
leobtt <leonarbat8@gmail.com>
2022-04-10 22:30:54 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -6,9 +8,22 @@ export default function Home() {
     <Head>
       <title>Ínicio | Ig.news</title>
     </Head>
-    <div>
-      <h1 >Hello World!!</h1>
-    </div>
+    <main className={styles.containerHome}>
+      <section className={styles.hero}>
+        <span>👏 Seja Bem-vindo </span>
+        <h1>
+          Novidades sobre o ecossistema
+          <span> React</span>
+          
+        </h1>
+        <p>
+          Tenha acesso a todas as publicações
+        </p>
+        <span>por R$ 9,90 por mês</span>
+        <SubscribeButton/>
+      </section>
+      <img src="/images/avatar.svg" alt="Garota codando" />
+    </main>
     </>
   )
 }
